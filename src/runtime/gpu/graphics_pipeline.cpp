06d17ae0a635b68ae587d28f2a072e901afb2ea7
Colby <me@cobeh.com>
2022-12-06 15:33:57 -0800

    finished graphics pipeline frontend

---
+++ src/runtime/gpu/graphics_pipeline.cpp
@@ -0,0 +1,21 @@
+// Copyright Colby Hall. All Rights Reserved.
+
+#include <gpu/context.hpp>
+#include <gpu/d3d12/d3d12_graphics_pipeline.hpp>
+
+GPU_NAMESPACE_BEGIN
+
+GraphicsPipeline GraphicsPipeline::make(GraphicsPipelineConfig&& config) {
+	auto& context = Context::the();
+
+	Option<Shared<GraphicsPipelineInterface, SMode::Atomic>> interface;
+	switch (context.backend()) {
+		case Backend::D3D12:
+			interface = make_shared<D3D12GraphicsPipeline, SMode::Atomic>(core::forward<GraphicsPipelineConfig>(config));
+			break;
+	}
+
+	return GraphicsPipeline { interface.unwrap() };
+}
+
+GPU_NAMESPACE_END
